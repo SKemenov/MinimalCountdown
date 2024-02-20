10924a825dcc96287e129c811a789de5bca9a0aa
Sergey Kemenov <skemenov@icloud.com>
2024-02-21 00:13:07 +0300

    Initial Commit

---
+++ MinimalCountdown/MinimalCountdownView.m
@@ -0,0 +1,51 @@
+//
+//  MinimalCountdownView.m
+//  MinimalCountdown
+//
+//  Created by Sergey Kemenov on 21.02.2024.
+//
+
+#import "MinimalCountdownView.h"
+
+@implementation MinimalCountdownView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
