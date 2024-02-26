c5e5c2fcef9b84f280e075d3288f086199f62746
Sergey Kemenov <skemenov@icloud.com>
2024-02-26 15:11:32 +0300

    Added `CGFloat` custom properties

---
+++ MinimalCountdown/Extensions/CGFloat+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  CGFloat+Extensions.swift
+//  MinimalCountdown
+//
+//  Created by Sergey Kemenov on 26.02.2024.
+//
+
+import AppKit
+
+extension CGFloat {
+    // MARK: - Custom properties
+
+    static let elementsWithSpaces: CGFloat = 7
+    static let elementsSpacingRatio: CGFloat = 0.04
+    static let verticalSpacing: CGFloat = -8
+    static let normalBright = (digits: 1.0, texts: 0.7)
+    static let dimBright = (digits: 0.8, texts: 0.5)
+}
