fb3856057daaad0d88dd2fcd7b5941c3776cc199
Sergey Kemenov <skemenov@icloud.com>
2024-03-03 13:49:18 +0300

    Refactored `ConfigureViewController` and static properties

--- MinimalCountdown/ConfigureViewController.swift
+++ MinimalCountdown/ConfigureViewController.swift
@@ -11,28 +11,27 @@ import AppKit
 final class ConfigureViewController: NSViewController {
     // MARK: - UI element's properties
 
-    let messageLabel = NSTextField(labelWithString: Resources.preferences[0])
-    let colorLabel = NSTextField(labelWithString: Resources.preferences[1])
-    let dateLabel = NSTextField(labelWithString: Resources.preferences[2])
-    let styleLabel = NSTextField(labelWithString: Resources.preferences[3])
-    let daysLabel = NSTextField(labelWithString: Resources.elements[0].capitalized)
-    let hoursLabel = NSTextField(labelWithString: Resources.elements[1].capitalized)
-    let minutesLabel = NSTextField(labelWithString: Resources.elements[2].capitalized)
-    let secondsLabel = NSTextField(labelWithString: Resources.elements[3].capitalized)
+    private let messageLabel = NSTextField(labelWithString: Resources.preferenceLabels[0])
+    private let colorLabel = NSTextField(labelWithString: Resources.preferenceLabels[1])
+    private let dateLabel = NSTextField(labelWithString: Resources.preferenceLabels[2])
+    private let styleLabel = NSTextField(labelWithString: Resources.preferenceLabels[3])
+    private let daysLabel = NSTextField(labelWithString: Resources.elements[0].capitalized)
+    private let hoursLabel = NSTextField(labelWithString: Resources.elements[1].capitalized)
+    private let minutesLabel = NSTextField(labelWithString: Resources.elements[2].capitalized)
+    private let secondsLabel = NSTextField(labelWithString: Resources.elements[3].capitalized)
 
-    let okButton = NSButton()
-    let cancelButton = NSButton()
+    private let okButton = NSButton()
+    private let cancelButton = NSButton()
 
     // MARK: - User input element's properties
 
-    var messageField = NSTextField()
-    var messageCheckbox = NSButton()
-    var dimCheckbox = NSButton()
-    var colorPopupButton = NSPopUpButton()
-    var datePicker = NSDatePicker()
-    var styleSlider = NSSlider()
+    private var messageField = NSTextField()
+    private var messageCheckbox = NSButton()
+    private var dimCheckbox = NSButton()
+    private var colorPopupButton = NSPopUpButton()
+    private var datePicker = NSDatePicker()
+    private var styleSlider = NSSlider()
 
-    // MARK: - Store properties
     // MARK: - Private properties
     private var screenSaverDefaults = ScreenSaverDefaults()
 
@@ -52,12 +51,18 @@ final class ConfigureViewController: NSViewController {
 
 private extension ConfigureViewController {
     func configureValues() {
+        colorPopupButton.removeAllItems()
+        colorPopupButton.addItems(withTitles: Resources.colorNames)
+
         colorPopupButton.selectItem(at: screenSaverDefaults.colorIndex)
         styleSlider.integerValue = screenSaverDefaults.showElementsIndex
         messageField.stringValue = screenSaverDefaults.messageString
         messageCheckbox.state = !screenSaverDefaults.messageIsHidden ? .on : .off
         dimCheckbox.state = !screenSaverDefaults.brightIsNormal ? .on : .off
         datePicker.dateValue = screenSaverDefaults.targetDate
+
+        datePicker.minDate = Date(timeIntervalSinceNow: 60 * 60 * 24 * 365 * -1)
+        datePicker.maxDate = Date(timeIntervalSinceNow: 60 * 60 * 24 * 365 * 1.5)
     }
 
     func configureUI() {
@@ -76,12 +81,18 @@ private extension ConfigureViewController {
             button.bezelStyle = .push
             button.setButtonType(.momentaryPushIn)
         }
+
+        [messageCheckbox, dimCheckbox].forEach { checkbox in
+            checkbox.bezelStyle = .push
+            checkbox.setButtonType(.switch)
+        }
         okButton.title = "OK"
         okButton.keyEquivalent = "\r"
+        okButton.action = #selector(okButtonClicked)
 
         cancelButton.title = "Cancel"
+        cancelButton.action = #selector(cancelButtonClicked)
 
-        styleSlider.trackFillColor = .blue
         styleSlider.minValue = 0
         styleSlider.maxValue = 3
         styleSlider.allowsTickMarkValuesOnly = true
@@ -89,132 +100,78 @@ private extension ConfigureViewController {
         styleSlider.tickMarkPosition = .below
 
         messageField.isEditable = true
+        messageField.isSelectable = true
+        messageField.becomeFirstResponder()
         messageField.placeholderString = "Input your message here"
-        messageField.stringValue = messageFieldValue
-
-        [messageCheckbox, dimCheckbox].forEach { checkbox in
-            checkbox.bezelStyle = .push
-            checkbox.setButtonType(.switch)
-        }
-        messageCheckbox.state = messageCheckboxValue ? .on : .off
         messageCheckbox.title = "Show the message"
+        dimCheckbox.title = "Dim the accent color"
 
-        dimCheckbox.state = dimCheckboxValue ? .on : .off
-        dimCheckbox.title = "Dim accent color"
-
-        colorPopupButton.removeAllItems()
-        colorPopupButton.addItems(withTitles: Resources.colorSet)
-        colorPopupButton.selectItem(at: colorPopupButtonIndex)
-
-        datePicker.dateValue = datePickerValue
         datePicker.datePickerStyle = .textField
         datePicker.presentsCalendarOverlay = true
         datePicker.datePickerMode = .single
-        datePicker.minDate = Date(timeIntervalSinceNow: 60 * 60 * 24 * 365 * -1)
-        datePicker.maxDate = Date(timeIntervalSinceNow: 60 * 60 * 24 * 365 * 1.5)
         datePicker.calendar = .current
-
-        okButton.action = #selector(okButtonClicked)
-        cancelButton.action = #selector(cancelButtonClicked)
-        styleSlider.action = #selector(styleSliderClicked(_:))
-        messageField.action = #selector(messageFieldFilled(_:))
-        messageCheckbox.action = #selector(messageCheckboxClicked(_:))
-        dimCheckbox.action = #selector(dimCheckboxClicked(_:))
-        colorPopupButton.action = #selector(colorPopupButtonClicked(_:))
-        datePicker.action = #selector(DatePickerClicked(_:))
     }
 
     func configureConstraints() {
-        let space: CGFloat = 16
-        let smallSpace: CGFloat = 4
-        let leading: CGFloat = 24
-
         NSLayoutConstraint.activate([
-            messageLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: leading),
-            messageLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: space),
-            messageLabel.widthAnchor.constraint(equalToConstant: 72),
+            messageLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: .leading),
+            messageLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .leading),
+            messageLabel.widthAnchor.constraint(equalToConstant: .labelWidth),
 
             messageField.centerYAnchor.constraint(equalTo: messageLabel.centerYAnchor),
-            messageField.leadingAnchor.constraint(equalTo: messageLabel.trailingAnchor, constant: space),
-            messageField.widthAnchor.constraint(equalToConstant: 256),
+            messageField.leadingAnchor.constraint(equalTo: messageLabel.trailingAnchor, constant: .smallLeading),
+            messageField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -.leading),
 
-            messageCheckbox.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: space),
-            messageCheckbox.leadingAnchor.constraint(equalTo: messageLabel.trailingAnchor, constant: space),
-            messageCheckbox.widthAnchor.constraint(equalToConstant: 168),
+            messageCheckbox.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: .space),
+            messageCheckbox.leadingAnchor.constraint(equalTo: messageLabel.trailingAnchor, constant: .smallLeading),
 
-            colorLabel.topAnchor.constraint(equalTo: messageCheckbox.bottomAnchor, constant: space),
-            colorLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: space),
-            colorLabel.widthAnchor.constraint(equalToConstant: 72),
+            colorLabel.topAnchor.constraint(equalTo: messageCheckbox.bottomAnchor, constant: .space),
+            colorLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .leading),
+            colorLabel.widthAnchor.constraint(equalToConstant: .labelWidth),
 
             colorPopupButton.centerYAnchor.constraint(equalTo: colorLabel.centerYAnchor),
-            colorPopupButton.leadingAnchor.constraint(equalTo: colorLabel.trailingAnchor, constant: space),
-            //            colorPopupButton.widthAnchor.constraint(equalToConstant: 168),
+            colorPopupButton.leadingAnchor.constraint(equalTo: colorLabel.trailingAnchor, constant: .smallLeading),
 
-            dimCheckbox.topAnchor.constraint(equalTo: colorLabel.bottomAnchor, constant: space),
-            dimCheckbox.leadingAnchor.constraint(equalTo: colorLabel.trailingAnchor, constant: space),
-            dimCheckbox.widthAnchor.constraint(equalToConstant: 168),
+            dimCheckbox.topAnchor.constraint(equalTo: colorLabel.bottomAnchor, constant: .space),
+            dimCheckbox.leadingAnchor.constraint(equalTo: colorLabel.trailingAnchor, constant: .smallLeading),
 
-            dateLabel.topAnchor.constraint(equalTo: dimCheckbox.bottomAnchor, constant: space),
-            dateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: space),
-            dateLabel.widthAnchor.constraint(equalToConstant: 72),
+            dateLabel.topAnchor.constraint(equalTo: dimCheckbox.bottomAnchor, constant: .space),
+            dateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .leading),
+            dateLabel.widthAnchor.constraint(equalToConstant: .labelWidth),
 
             datePicker.centerYAnchor.constraint(equalTo: dateLabel.centerYAnchor),
-            datePicker.leadingAnchor.constraint(equalTo: dateLabel.trailingAnchor, constant: space),
+            datePicker.leadingAnchor.constraint(equalTo: dateLabel.trailingAnchor, constant: .smallLeading),
 
-            styleLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: space),
-            styleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: space),
-            styleLabel.widthAnchor.constraint(equalToConstant: 72),
+            styleLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: .space),
+            styleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .leading),
+            styleLabel.widthAnchor.constraint(equalToConstant: .labelWidth),
 
             styleSlider.centerYAnchor.constraint(equalTo: styleLabel.centerYAnchor),
-            styleSlider.leadingAnchor.constraint(equalTo: styleLabel.trailingAnchor, constant: space),
-            styleSlider.widthAnchor.constraint(equalToConstant: 256),
+            styleSlider.leadingAnchor.constraint(equalTo: styleLabel.trailingAnchor, constant: .smallLeading),
+            styleSlider.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -.leading),
 
-            daysLabel.topAnchor.constraint(equalTo: styleSlider.bottomAnchor, constant: smallSpace),
-            daysLabel.leadingAnchor.constraint(equalTo: styleSlider.leadingAnchor, constant: -8),
+            daysLabel.topAnchor.constraint(equalTo: styleSlider.bottomAnchor, constant: .smallSpace),
+            daysLabel.leadingAnchor.constraint(equalTo: styleSlider.leadingAnchor),
 
-            hoursLabel.topAnchor.constraint(equalTo: styleSlider.bottomAnchor, constant: smallSpace),
-            hoursLabel.leadingAnchor.constraint(equalTo: styleSlider.leadingAnchor, constant: 70),
+            hoursLabel.topAnchor.constraint(equalTo: styleSlider.bottomAnchor, constant: .smallSpace),
+            hoursLabel.leadingAnchor.constraint(equalTo: styleSlider.leadingAnchor, constant: 80),
 
-            minutesLabel.topAnchor.constraint(equalTo: styleSlider.bottomAnchor, constant: smallSpace),
-            minutesLabel.leadingAnchor.constraint(equalTo: styleSlider.leadingAnchor, constant: 144),
+            minutesLabel.topAnchor.constraint(equalTo: styleSlider.bottomAnchor, constant: .smallSpace),
+            minutesLabel.leadingAnchor.constraint(equalTo: styleSlider.leadingAnchor, constant: 165),
 
-            secondsLabel.topAnchor.constraint(equalTo: styleSlider.bottomAnchor, constant: smallSpace),
-            secondsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -leading),
+            secondsLabel.topAnchor.constraint(equalTo: styleSlider.bottomAnchor, constant: .smallSpace),
+            secondsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -.leading),
 
-            okButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -leading),
-            okButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -leading),
-            okButton.widthAnchor.constraint(equalToConstant: 72),
-            okButton.heightAnchor.constraint(equalToConstant: 24),
+            okButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -.leading),
+            okButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -.leading),
+            okButton.widthAnchor.constraint(equalToConstant: .buttonWidth),
+            okButton.heightAnchor.constraint(equalToConstant: .buttonHeight),
 
-            cancelButton.bottomAnchor.constraint(equalTo: okButton.bottomAnchor, constant: 0),
-            cancelButton.trailingAnchor.constraint(equalTo: okButton.leadingAnchor, constant: -space),
-            cancelButton.widthAnchor.constraint(equalToConstant: 72),
-            cancelButton.heightAnchor.constraint(equalToConstant: 24),
+            cancelButton.bottomAnchor.constraint(equalTo: okButton.bottomAnchor),
+            cancelButton.trailingAnchor.constraint(equalTo: okButton.leadingAnchor, constant: -.space),
+            cancelButton.widthAnchor.constraint(equalToConstant: .buttonWidth),
+            cancelButton.heightAnchor.constraint(equalToConstant: .buttonHeight),
         ])
-    }
-
-    @objc func colorPopupButtonClicked(_ sender: NSPopUpButton) {
-        colorPopupButtonIndex = sender.indexOfSelectedItem
-    }
-
-    @objc func messageFieldFilled(_ sender: NSTextField) {
-//        messageFieldValue = sender.stringValue
-    }
-
-    @objc func messageCheckboxClicked(_ sender: NSButton) {
-        messageCheckboxValue = sender.state.rawValue == 1 ? true : false
-    }
-
-    @objc func dimCheckboxClicked(_ sender: NSButton) {
-        dimCheckboxValue = sender.state.rawValue == 1 ? true : false
-    }
-
-    @objc func DatePickerClicked(_ sender: NSDatePicker) {
-        datePickerValue = sender.dateValue
-    }
-
-    @objc func styleSliderClicked(_ sender: NSSlider) {
-//        print(#function, sender.tickMarkPosition)
     }
 
     func saveData() {
@@ -232,6 +189,7 @@ private extension ConfigureViewController {
     }
 
     @objc func okButtonClicked() {
+        messageField.resignFirstResponder()
         saveData()
         closeSheet()
     }
