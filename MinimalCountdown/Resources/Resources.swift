c2db7dc5e7017dfb28295e6194e0c174bdb39070
Sergey Kemenov <skemenov@icloud.com>
2024-05-25 14:00:00 +0300

    add Labels in Resources

--- MinimalCountdown/Resources/Resources.swift
+++ MinimalCountdown/Resources/Resources.swift
@@ -13,6 +13,17 @@ enum Resources {
     static let settingsFolderName = ".minimalCountdown"
     static let settingsFileName = "settings.json"
 
+    enum Labels {
+        static let MessageTitle = "Message:"
+        static let ColorTitle = "Color:"
+        static let DateTitle = "Date:"
+        static let StyleTitle = "Style:"
 
+        static let messagePlaceholder = "Input your message here"
+        static let messageCheckboxTitle = "Show the message"
+        static let dimCheckboxTitle = "Dim the accent color"
 
+        static let OkButtonTitle = "OK"
+        static let CancelButtonTitle = "Cancel"
+    }
 }
