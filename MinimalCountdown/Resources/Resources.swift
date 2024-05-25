b85de9d41c96d8ddc9072d462e06c148eb2d8f59
Sergey Kemenov <skemenov@icloud.com>
2024-05-25 11:30:00 +0300

    remove old Resources arrays

--- MinimalCountdown/Resources/Resources.swift
+++ MinimalCountdown/Resources/Resources.swift
@@ -5,62 +5,12 @@
 //  Created by Sergey Kemenov
 //
 
-import AppKit
+import Foundation
 
 enum Resources {
     // MARK: - Dictionaries
 
-    enum ScreensaverState: Int {
-        case showDays
-        case showDaysHours
-        case showDaysHoursMinutes
-        case showAll
-    }
 
-    static let elements = [
-        "days",
-        "hours",
-        "minutes",
-        "seconds"
-    ]
 
-    static let preferenceLabels = [
-        "Message:",
-        "Color:",
-        "Date:",
-        "Style:"
-    ]
 
-    static let colorNames = [
-        "White",
-        "Red",
-        "Pink",
-        "Orange",
-        "Yellow",
-        "Green",
-        "Mint",
-        "Cyan",
-        "Blue",
-        "Indigo",
-        "Purple"
-    ]
-
-    static let colors: [NSColor] = [
-        .white,
-        .systemRed,
-        .systemPink,
-        .systemOrange,
-        .systemYellow,
-        .systemGreen,
-        .systemMint,
-        .cyan,
-        .systemBlue,
-        .systemIndigo,
-        .systemPurple
-    ]
-
-    static let backgroundColors: [NSColor] = [
-        .black,
-        .white
-    ]
 }
