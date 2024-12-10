548661656401e535d2e124cc3bcc1bfdbaa1fcd2
Sergey Kemenov <skemenov@icloud.com>
2024-12-10 15:03:45 +0300

    update colors' models

--- MinimalCountdown/Models/AccentColor.swift
+++ MinimalCountdown/Models/AccentColor.swift
@@ -7,27 +7,32 @@
 
 import AppKit
 
-enum AccentColor: Int, CaseIterable, CaseNameable, SafeIntDecodable {
+enum AccentColor: Int, CaseIterable, ColorExtendable, SafeIntDecodable {
+//enum AccentColor: Int, CaseIterable, ColorExtendable, SafeIntDecodable, Identifiable, Equatable, Hashable {
     case white = 0, red, pink, orange, yellow, green, mint, cyan, blue, indigo, purple
-
-    var color: NSColor {
-        switch self {
-        case .white:  return .white
-        case .red:    return .systemRed
-        case .pink:   return .systemPink
-        case .orange: return .systemOrange
-        case .yellow: return .systemYellow
-        case .green:  return .systemGreen
-        case .mint:   return .systemMint
-        case .cyan:   return .cyan
-        case .blue:   return .systemBlue
-        case .indigo: return .systemIndigo
-        case .purple: return .systemPurple
-        }
-    }
 
     // safe and un-optional, defaults to .white
     init(_ rawValue: Int) {
         self = AccentColor(rawValue: rawValue) ?? .white
     }
+}
+
+extension AccentColor {
+    var nsColor: NSColor {
+        switch self {
+            case .white: .white
+            case .red: .systemRed
+            case .pink: .systemPink
+            case .orange: .systemOrange
+            case .yellow: .systemYellow
+            case .green: .systemGreen
+            case .mint: .systemMint
+            case .cyan: .cyan
+            case .blue: .systemBlue
+            case .indigo: .systemIndigo
+            case .purple: .systemPurple
+        }
+    }
+
+    var id: Int { self.rawValue }
 }
