6b5320d0198e4f8937b12474bb9c088bc4d643ae
Sergey Kemenov <skemenov@icloud.com>
2024-12-10 10:37:12 +0300

    update `styleElement` model

--- MinimalCountdown/Models/StyleElement.swift
+++ MinimalCountdown/Models/StyleElement.swift
@@ -9,11 +9,26 @@ import Foundation
 
 // MARK: - Enums
 
-enum StyleElement: Int, CaseIterable, CaseNameable, SafeIntDecodable {
+enum StyleElement: Int, CaseIterable, Identifiable, SafeIntDecodable {
     case days = 0, hours, minutes, seconds
 
     // safe and un-optional, defaults to .seconds (show all)
     init(_ rawValue: Int) {
         self = StyleElement(rawValue: rawValue) ?? .seconds
+    }
+}
+
+extension StyleElement {
+    var id: Self { self }
+    
+    var label: String {
+        String(describing: self).uppercased()
+    }
+
+    var menuLabel: String {
+        switch self {
+            case .days: String(describing: self).capitalized
+            default: "+ " + String(describing: self).capitalized
+        }
     }
 }
