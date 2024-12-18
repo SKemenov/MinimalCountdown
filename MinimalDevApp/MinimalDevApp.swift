af8cea08e70663b5c5da663f299327dddbabce81
Sergey Kemenov <skemenov@icloud.com>
2024-12-18 11:43:56 +0300

    remove AppDelegate from DevApp

--- MinimalDevApp/MinimalDevApp.swift
+++ MinimalDevApp/MinimalDevApp.swift
@@ -9,8 +9,6 @@ import SwiftUI
 
 @main
 struct MinimalDevApp: App {
-    @NSApplicationDelegateAdaptor(DevAppDelegate.self) var appDelegate
-
     var body: some Scene {
         WindowGroup("MinimalCountdown Debug Preview") {
             ScreenSaverRepresentable()
@@ -27,28 +25,3 @@ struct MinimalDevApp: App {
     }
 }
 
-class DevAppDelegate: NSObject, NSApplicationDelegate {
-    private var settingsManager: SettingsManager?
-    private var settingsSheet: NSWindow?
-
-    @objc func openSettings() {
-        guard
-            let mainWindow = NSApp.mainWindow ?? NSApp.windows.first,
-            let fileStore = FileStore(),
-            let defaultsStore = DefaultsStore() else { return }
-
-        let manager = SettingsManager(stores: [fileStore, defaultsStore])
-        manager.load()
-        settingsManager = manager
-
-        let viewController = ConfigureViewController(settingsManager: manager)
-        let sheet = NSWindow(contentViewController: viewController)
-        sheet.title = "Settings"
-        settingsSheet = sheet
-
-        mainWindow.beginSheet(sheet) { [weak self] _ in
-            self?.settingsSheet = nil
-            self?.settingsManager = nil
-        }
-    }
-}
