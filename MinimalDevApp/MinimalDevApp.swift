81f26b76ac05d208b3964432f5d809e6288c49f3
Sergey Kemenov <skemenov@icloud.com>
2024-12-23 09:34:18 +0300

    add SettingsManager into DevApp

--- MinimalDevApp/MinimalDevApp.swift
+++ MinimalDevApp/MinimalDevApp.swift
@@ -9,12 +9,30 @@ import SwiftUI
 
 @main
 struct MinimalDevApp: App {
+    private var settingsManager: SettingsManager
+
     @AppStorage("isAnimating") var isAnimating: Bool = false
+
+    init() {
+        settingsManager = SettingsManager(stores: Self.createStores())
+    }
+
     var body: some Scene {
         WindowGroup("Minimal Countdown Preview") {
             ScreenSaverRepresentable(isAnimating: $isAnimating)
         }
         .windowResizability(.contentSize)
      }
+    }
+}
+
+private extension MinimalDevApp {
+    static func createStores() -> [LocalStore] {
+        if let fileStore = FileStore(), let defaultsStore = DefaultsStore() {
+            return [fileStore, defaultsStore]
+        } else {
+            return []
+        }
+    }
 }
 
