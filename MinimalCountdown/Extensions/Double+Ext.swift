bcab85479f131d31609853b9a359f2be460224d8
Sergey Kemenov <skemenov@icloud.com>
2025-02-12 20:48:55 +0300

    refactor(ext): add Double static properties for SwiftUI views

--- MinimalCountdown/Extensions/Double+Ext.swift
+++ MinimalCountdown/Extensions/Double+Ext.swift
@@ -13,4 +13,10 @@ extension Double {
     static let oneDay = 60.0 * 60 * 24
     /// Total seconds in an year and one day: 31 622 400.0 = 60.0 * 60 * 24 * (365 + 1)
     static let oneYearAndOneDay = 60.0 * 60 * 24 * (365 + 1)
+
+    // MARK: - Color properties
+    /// CGFloat = (digits: 1.0, texts: 0.7)
+    static let normalBright = (digits: 1.0, texts: 0.7)
+    /// CGFloat = (digits: 0.8, texts: 0.5)
+    static let dimBright = (digits: 0.8, texts: 0.5)
 }
