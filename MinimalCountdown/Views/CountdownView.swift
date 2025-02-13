d8ec5dfac210db44a82b5f924b2f668a60335d05
Sergey Kemenov <skemenov@icloud.com>
2025-02-13 19:42:08 +0300

    feat(views): create base `CountdownView` SwiftUI view

---
+++ MinimalCountdown/Views/CountdownView.swift
@@ -0,0 +1,57 @@
+//
+//  CountdownView.swift
+//  MinimalCountdown
+//
+//  Created by Sergey Kemenov
+//
+
+import SwiftUI
+
+struct CountdownView: View {
+    let now: Date
+    let settings: SaverSettings
+    let windowWidth: CGFloat
+    var isPreview: Bool = false
+
+    var body: some View {
+        HStack(
+            alignment: .top,
+            spacing: windowWidth.calculateSize(for: .spacing, isPreview: isPreview)
+        ) {
+            ForEach(visibleElements) { style in
+                ElementView(
+                    digits: digits(for: style),
+                    label: style.label,
+                    size: windowWidth.calculateSize(for: .digits, isPreview: isPreview),
+                    digitsColor: settings.digitsColor,
+                    textsColor: settings.textsColor
+                )
+            }
+        }
+    }
+}
+
+private extension CountdownView {
+    var visibleElements: [StyleElement] {
+        switch settings.style {
+            case .days: [.days]
+            case .hours: [.days, .hours]
+            case .minutes: [.days, .hours, .minutes]
+            case .seconds: StyleElement.allCases
+        }
+    }
+
+    func digits(for style: StyleElement) -> String {
+        switch style {
+            case .days: settings.targetDate.daysString(relativeTo: now)
+            case .hours: settings.targetDate.hoursString(relativeTo: now)
+            case .minutes: settings.targetDate.minutesString(relativeTo: now)
+            case .seconds: settings.targetDate.secondsString(relativeTo: now)
+        }
+    }
+}
+
+#Preview {
+    CountdownView(now: Date(), settings: SaverSettings.default, windowWidth: 500)
+        .frame(width: 500, height: 300)
+}
