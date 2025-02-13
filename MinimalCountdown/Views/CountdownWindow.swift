9dbd25a6e010b580985a1a477ea233383168ebe9
Sergey Kemenov <skemenov@icloud.com>
2025-02-13 21:14:33 +0300

    feat(views): create `CountdownWindow` SwiftUI view

---
+++ MinimalCountdown/Views/CountdownWindow.swift
@@ -0,0 +1,54 @@
+//
+//  CountdownWindow.swift
+//  MinimalCountdown
+//
+//  Created by Sergey Kemenov
+//
+
+import SwiftUI
+
+struct CountdownWindow: View {
+    let now: Date
+    let settings: SaverSettings
+    var isPreview: Bool = false
+
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                settings.backgroundColor.color
+                    .ignoresSafeArea()
+
+                VStack(spacing: .zero) {
+                    TitleView(
+                        settings: settings,
+                        textSize: geo.size.width.calculateSize(for: .title, isPreview: isPreview)
+                    )
+                    CountdownView(
+                        now: now,
+                        settings: settings,
+                        windowWidth: geo.size.width,
+                        isPreview: isPreview
+                    )
+                }
+                .frame(width: geo.size.width, height: geo.size.height)
+            }
+        }
+    }
+}
+
+#Preview("Default — 30 days out") {
+    CountdownWindow(
+        now: Date(),
+        settings: .default
+    )
+    .frame(width: 500, height: 300)
+}
+
+#Preview("Default — for Preview") {
+    CountdownWindow(
+        now: Date(),
+        settings: .default,
+        isPreview: true
+    )
+    .frame(width: 500, height: 300)
+}
