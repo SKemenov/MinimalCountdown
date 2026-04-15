58bc4a7c9d731a1029b0bd4a6cd6d250f6e6d2a3
Sergey Kemenov <skemenov@icloud.com>
2026-04-15 17:36:02 +0300

    refactor(models): collapse StyleElement.appearanceLabel into additive one-liner

--- MinimalCountdown/Models/StyleElement.swift
+++ MinimalCountdown/Models/StyleElement.swift
@@ -30,11 +30,7 @@ extension StyleElement {
     }
 
     var appearanceLabel: String {
-        switch self {
-            case .days: Self.days.string
-            case .hours: [Self.days, Self.hours].map { $0.string }.formatted(.list(type: .and))
-            case .minutes: [Self.days, Self.hours, Self.minutes].map { $0.string }.formatted(.list(type: .and))
-            case .seconds: "All Elements"
-        }
+        (self == .days ? String(describing: self) : "+ " + String(describing: self))
+            .capitalized
     }
 }
