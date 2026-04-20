939d9ea2d6155932a9be47618585e939c0bf5d2e
Sergey Kemenov <skemenov@icloud.com>
2026-04-20 10:30:00 +0300

    feat(settings): add LocalStoreError typed enum

--- MinimalCountdown/Settings/LocalStore.swift
+++ MinimalCountdown/Settings/LocalStore.swift
@@ -11,3 +11,25 @@ protocol LocalStore {
     func load() -> SaverSettings?
     func save(_ settings: SaverSettings)
 }
+
+enum LocalStoreError: Error, LocalizedError {
+    case directoryUnavailable(url: URL, underlying: Error)
+    case encodingFailed(underlying: Error)
+    case writeFailed(url: URL, underlying: Error)
+    case defaultsSyncFailed
+
+    var logDescription: String {
+        switch self {
+            case .directoryUnavailable(let url, let underlying):
+                "Could not create settings directory at \(url.path): \(underlying.localizedDescription)"
+            case .encodingFailed(let underlying):
+                "Could not encode settings: \(underlying.localizedDescription)"
+            case .writeFailed(let url, let underlying):
+                "Could not write settings to \(url.path): \(underlying.localizedDescription)"
+            case .defaultsSyncFailed:
+                "ScreenSaverDefaults.synchronize() returned false"
+        }
+    }
+
+    var errorDescription: String? { logDescription }
+}
